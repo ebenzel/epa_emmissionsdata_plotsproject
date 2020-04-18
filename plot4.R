91f297bdcc3a5d0ab70d5764a364829d7a4cc921
ebenzel <ericbenzel@gmail.com>
2020-04-18 16:56:43 -0600

    Plot 4 Final

--- plot4.R
+++ plot4.R
@@ -1,2 +1,25 @@
 # Question: Across the United States, how have emissions from coal combustion-related sources 
 # changed from 1999–2008?
+library(tidyverse)
+
+NEI <- read_rds("summarySCC_PM25.rds")
+SCC <- read_rds("Source_Classification_Code.rds")
+
+# select coal combusion-related sources by filtering SCC by EI.Sector that includes "Fuel Comb" and "Coal"
+coalSCC <- SCC %>% 
+        filter(str_detect(EI.Sector, "Coal$")) %>%
+        select(SCC)
+
+coal.totals <- NEI %>%
+        filter(SCC %in% coalSCC$SCC) %>%
+        group_by(year) %>%
+        summarize(total = sum(Emissions))
+
+coal.totals %>%
+        ggplot(aes(year, total))+
+        geom_point(size = 2) +
+        labs(x = "Year", y = "PM2.5 Emmission (tons)", title = "Coal Combustion Related PM2.5 Emissions")
+
+# create plot
+dev.copy(png,"plot4.png")
+dev.off()
